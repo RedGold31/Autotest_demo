a2ced6edb64dc9e037667482e9954ccdb15b1251
RedGold31 <max10050031@gmail.com>
2024-08-08 14:49:08 +0300

    Уже что то похожее на тестировку функции сайта.

--- Vivus_autotest/Vivus_test.py
+++ Vivus_autotest/Vivus_test.py
@@ -8,11 +8,19 @@ options.add_experimental_option("excludeSwitches", ["enable-logging"])
 service = Service(executable_path=r'C:\Users\mpopov\Desktop\edgedriver_win64\msedgedriver.exe')    # подключение вебдрайвера
 driver = webdriver.Edge(service=service, options=options)
 driver.get("https://www.vivus.ru/")    # открытие браузера с нужной страницей
+driver.set_window_size(1600, 900) # размер окна (По желанию)
 
 
-original_window = driver.current_window_handle
+original_window = driver.current_window_handle      # переменная для хранения главной вкладки сайта
+url_original_window = driver.current_url
+count = ""
+# for i in range(10):
 for a in range(1, 6):       # Пробежка по нужной колонке на сайте
     element = driver.find_element(By.XPATH, f'/html/body/footer/div/div/div[1]/p/a[{a}]')     # поиск элемента страницы с помощью XPATH
+    link = element.get_attribute('href')
+    assert link != count
+    print('Прошлый: ', count,  '\n', 'Текущий: ', link, '\n *****************************')
+    count = link
     driver.execute_script("arguments[0].scrollIntoView(true);", element)    # прокрутка страницы до определенного элемента
     time.sleep(2)
     element.click()     # прожатие элемента
@@ -20,6 +28,7 @@ for a in range(1, 6):       # Пробежка по нужной колонке 
     driver.switch_to.window(original_window) # переключение на основную вкладку браузера
 
 
-time.sleep(5)
+time.sleep(2)
 driver.quit()     # закрытие браузера
+
 exit()
