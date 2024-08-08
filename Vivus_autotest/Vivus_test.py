91357b5fbfb493da30d5012a5a691f56f93413a9
RedGold31 <max10050031@gmail.com>
2024-08-08 14:54:23 +0300

    Уже что то похожее на тестировку функции сайта. + пофиксил комментарии

--- Vivus_autotest/Vivus_test.py
+++ Vivus_autotest/Vivus_test.py
@@ -12,13 +12,12 @@ driver.set_window_size(1600, 900) # размер окна (По желанию)
 
 
 original_window = driver.current_window_handle      # переменная для хранения главной вкладки сайта
-url_original_window = driver.current_url
 count = ""
 # for i in range(10):
 for a in range(1, 6):       # Пробежка по нужной колонке на сайте
     element = driver.find_element(By.XPATH, f'/html/body/footer/div/div/div[1]/p/a[{a}]')     # поиск элемента страницы с помощью XPATH
-    link = element.get_attribute('href')
-    assert link != count
+    link = element.get_attribute('href')    # поиск ссылки в элементе
+    assert link != count    # проверка на ошибку новой ссылки
     print('Прошлый: ', count,  '\n', 'Текущий: ', link, '\n *****************************')
     count = link
     driver.execute_script("arguments[0].scrollIntoView(true);", element)    # прокрутка страницы до определенного элемента
